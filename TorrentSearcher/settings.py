94f2ffd1382babdc4e0d2f46984a2ab2ac6527a9
Sascha Häusler <saeschdivara@gmail.com>
2014-06-22 13:33:30 +0200

    Moved static files to TorrentSearcher

--- TorrentSearcher/settings.py
+++ TorrentSearcher/settings.py
@@ -80,11 +80,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 # media deliver
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'TorrentSearcher/media')
 MEDIA_URL = '/media/'
 
 # static files (application js/img etc)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'TorrentSearcher/static')
 STATIC_URL = '/static/'
 
 # List of finder classes that know how to find static files in
