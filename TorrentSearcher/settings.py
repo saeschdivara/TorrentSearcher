2caf65a6b0b08860225b092b2154fffc90ed53ff
Sascha Häusler <saeschdivara@gmail.com>
2014-06-22 13:24:02 +0200

    Added static files

--- TorrentSearcher/settings.py
+++ TorrentSearcher/settings.py
@@ -76,11 +76,24 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+# media deliver
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+# static files (application js/img etc)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
