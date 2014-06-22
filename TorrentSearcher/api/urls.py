adf5c7571dba327217958a28557a9463f05b3078
Sascha Häusler <saeschdivara@gmail.com>
2014-06-22 21:03:37 +0200

    Adding api for rest login

--- TorrentSearcher/api/urls.py
+++ TorrentSearcher/api/urls.py
@@ -2,17 +2,10 @@ from django.conf.urls import url, patterns, include
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets, routers
 
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    model = User
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    model = Group
-
-
 # Routers provide an easy way of automatically determining the URL conf.
+from TorrentSearcher.api.authentication.views import UserViewSet, GroupViewSet
+
+
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
